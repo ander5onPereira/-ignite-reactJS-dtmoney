7a43530c5d022b45a507e83c29110a2f721e0ad8
Anderson Pereira <anderson.carro.95@gmail.com>
2021-12-04 00:49:22 -0300

    Chapter II - Componentização - Componente: Summary

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -5,14 +5,14 @@ export const GlobalStyle = createGlobalStyle`
     --background: #f0f2f5;
     --red: #E52E4D;
     --blue: #5429cc;
-
+    --green: #33CC95;
     --blue-light: #6933FF;
 
-    --text-title:#363F5F;
-    --text-body:#969CB3;
+    --text-title: #363F5F;
+    --text-body: #969CB3;
 
-    --background: #f0f2f5;
-    --shape:#FFFFFF;
+    --background: #F0F2F5;
+    --shape: #FFFFFF;
   }
 
   *{
@@ -29,7 +29,7 @@ export const GlobalStyle = createGlobalStyle`
     }
   }
   body{
-    background: var(---background);
+    background: var(--background);
     -webkit-font-smoothing: antialiased;
   }
 

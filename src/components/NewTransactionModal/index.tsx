6ccc015cbb0f2a961e0144d899ab89a55d7eb10d
Anderson Pereira <anderson.carro.95@gmail.com>
2022-02-20 14:38:39 -0300

    Chapter II - Modal & Forms - Cores dos botões

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -39,6 +39,7 @@ export function NewTransactionModal({
               setType('deposit');
             }}
             isActive={type === 'deposit'}
+            activeColor="green"
           >
             <img src={incomeImg} alt="Entrada" />
             <span>Entrada</span>
@@ -49,6 +50,7 @@ export function NewTransactionModal({
               setType('withdraw');
             }}
             isActive={type === 'withdraw'}
+            activeColor="red"
           >
             <img src={outcomeImg} alt="Saída" />
             <span>Saída</span>
